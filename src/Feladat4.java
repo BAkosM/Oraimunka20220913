b9b796708115f315033e92a1ff3e6dd74fd45ec2
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-14 17:10:44 +0200

    Feladat4

---
+++ src/Feladat4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Feladat4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adjameg a háromszög a oldalát: ");
+        double a = sc.nextDouble();
+        System.out.println("Adjameg a háromszög b oldalát: ");
+        double b = sc.nextDouble();
+        System.out.println("Adjameg a háromszög c oldalát: ");
+        double c = sc.nextDouble();
+        if(a<b+c && b<a+c && c<b+a){
+            Double k = a+b+c;
+            System.out.printf("A háromszög kerülete: %.0f", k);
+        }else{
+            System.out.println("Ez nem egy háromszög!");
+        }
+    }
+}
